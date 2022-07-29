63399e13b1421ea1e3eab472c96f78c9e3caf51e
Aleksandr Kostikov <alexko@microsoft.com>
2022-07-29 14:38:20 -0700

    playground added

---
+++ playground/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    print!("Hello, world!");
+    //println!("Is it zero? {}", 1. / f64::INFINITY);
+    //std::f64::consts::PI
+    //std::f32::consts::PI
+    //let mut v = vec![0; 10];
+    //v.push(2);
+
+
+}
