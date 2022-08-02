994f3312096ab0c43023510931bbe2dc0c23ecb8
Alexander Kostikov <Alex.Kostikov@gmail.com>
2022-08-01 19:07:50 -0700

    checking if rust can now handle const param expressions - nope, as per the book

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,7 @@
+struct Poly<const N: usize> {
+    coef: [f64; N]
+}
+
 fn main() {
     print!("Hello, world!");
     //println!("Is it zero? {}", 1. / f64::INFINITY);
@@ -7,7 +11,8 @@ fn main() {
     //v.push(2);
     //let mut f = "first".to_string();
     //f = "ddd".to_string();
+    //let mut v = vec![0; 10];
+    //std::mem::rep
 
-    let mut v = vec![0; 10];
-    std::mem::rep
+    //let ply = Poly<{5+1}>()
 }
