2ee5bde0799f6a96ed911821760112e9ae52500b
Aleksandr Kostikov <alexko@microsoft.com>
2022-09-01 12:35:15 -0700

    better names

--- web-chat/src/lib.rs
+++ web-chat/src/lib.rs
@@ -5,34 +5,34 @@ pub mod utils;
 
 // p569
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
-pub enum FromClient {
+pub enum ClientPacket {             // was:FromClient
     Join {
-        group_name: Arc<String>,
+        group: Arc<String>,         // was:group_name
     },
     Post {
-        group_name: Arc<String>,
+        group: Arc<String>,
         message: Arc<String>,
     },
 }
 
-pub enum FromServer {               // enum
+pub enum ServerPacket {             // was:FromServer, enum
     Message {                       // struct variant
-        group_name: Arc<String>,
+        group: Arc<String>,
         message: Arc<String>,
     },
     Error(String),                  // tuple variant
 }
 
 #[test]
-fn test_from_clinet_json_is_correct() {
+fn test_client_packet_json_is_correct() {
 
-    let target = FromClient::Post {
-        group_name: Arc::new("Cats".to_string()),
+    let target = ClientPacket::Post {
+        group: Arc::new("Cats".to_string()),
         message: Arc::new("Hello cats!".to_string()),
     };
 
     let serialized = serde_json::to_string(&target).unwrap();
-    let deserialized = serde_json::from_str::<FromClient>(&serialized).unwrap();
+    let deserialized = serde_json::from_str::<ClientPacket>(&serialized).unwrap();
 
     assert_eq!(serialized, "{\"Post\":{\"group_name\":\"Cats\",\"message\":\"Hello cats!\"}}");
     assert_eq!(serialized, r#"{"Post":{"group_name":"Cats","message":"Hello cats!"}}"#); // raw string p74
