79270e7554a4fe2990fe38257f4ebe3b504aa776
Aleksandr Kostikov <alexko@microsoft.com>
2022-09-07 12:20:52 -0700

    receive_packet added

--- web-chat/src/utils.rs
+++ web-chat/src/utils.rs
@@ -1,4 +1,4 @@
-use async_std::io::WriteExt;
+use async_std::{io::{WriteExt, prelude::BufReadExt}, stream::{Stream, StreamExt}};
 
 
 // In real apps use anyhow crate for generic thread-safe errors
@@ -12,7 +12,25 @@ where
     Packet: serde::Serialize
 {
     let mut json = serde_json::to_string(&packet)?;
+
+    // New line is used to separate commands for processing
     json.push('\n');
+
+    // We could have called flush here as well
+    // Right now this util method assumes that buffer is flushed upstream
     outbound.write_all(json.as_bytes()).await?;
+
     Ok(())
+}
+
+pub fn receive_packet<Stream, Packet>(inbound: Stream) -> impl async_std::prelude::Stream<Item = AppResult<Packet>>
+where
+    Stream: async_std::io::BufRead + Unpin,
+    Packet: serde::de::DeserializeOwned
+{
+    inbound.lines().map(|line_read| -> AppResult<Packet> {
+        let line = line_read?;
+        let packet = serde_json::from_str::<Packet>(&line)?;
+        Ok(packet)
+    })
 }
