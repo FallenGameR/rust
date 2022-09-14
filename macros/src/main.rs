779be2eca2da3773c3a070bbf21336f9f7efeec2
Aleksandr Kostikov <alexko@microsoft.com>
2022-09-14 12:21:33 -0700

    json macro implementation

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -10,6 +10,7 @@ enum Json
     Object(Box<HashMap<String, Json>>),
 }
 
+/// Construct Json representation for any Rust number type
 macro_rules! impl_from_num_to_json
 {
     ( $($t:ident)* ) => { $(
@@ -25,7 +26,27 @@ macro_rules! impl_from_num_to_json
 
 impl_from_num_to_json!(i8 u8 i16 u16 i32 u32 i64 u64 i128 u128 isize usize);
 
+/// Construct JSON via macro
+/// tt is token tree
+macro_rules! json
+{
+    (null) => {
+        Json::Null
+    };
+    ( [$($element:tt),*] ) => {
+        Json::Array(vec![ $( json!($element) ),* ])
+    };
+    ( {$($key:tt : $value:tt),*} ) => {
+        Json::Object(Box::new(vec![
+            $( ($key.to_string(), json!($value)) ),*
+        ].into_iter().collect()))
+    };
+    ( $other:tt ) => {
+        Json::from($other)
+    };
+}
 
 fn main() {
+    todo!("Add macro debugging info ");
     println!("Hello, world!");
 }
