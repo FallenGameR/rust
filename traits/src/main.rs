899b379ef84c5090b95a4f289b215d010106bc36
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-01-31 12:41:50 -0800

    trait added instead of direct call, like interface in C#

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use lol_html::{element, HtmlRewriter, Settings};
+use lol_html::{element, HtmlRewriter, Settings, OutputSink};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("input.html");
@@ -15,11 +15,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         },
         |c: &[u8]| output.extend_from_slice(c),
     )?;
-    rewriter.write(input.as_bytes())?;
-    rewriter.end()?;
+
+    process(input, &mut rewriter)?;
+    //rewriter.write(input.as_bytes())?;
+    //rewriter.end()?;
 
     println!("input: {input}");
     println!("output: {}", std::str::from_utf8(&output).unwrap());
 
+    Ok(())
+}
+
+trait Processor{
+    fn write(&mut self, chunk: &[u8]) -> Result<(), Box<dyn Error>>;
+    fn end(&mut self) -> Result<(), Box<dyn Error>>;
+}
+
+impl<'processor, Output: OutputSink> Processor for HtmlRewriter<'processor, Output>{
+    fn write(&mut self, chunk: &[u8]) -> Result<(), Box<dyn Error>> {
+        HtmlRewriter::write(self, chunk).map_err(Into::into)
+    }
+
+    fn end(&mut self) -> Result<(), Box<dyn Error>> {
+        HtmlRewriter::end(self).map_err(Into::into)
+    }
+}
+
+fn process(input: &str, processor: &mut dyn Processor) -> Result<(), Box<dyn Error>>{
+    processor.write(input.as_bytes())?;
+    processor.end()?;
     Ok(())
 }
